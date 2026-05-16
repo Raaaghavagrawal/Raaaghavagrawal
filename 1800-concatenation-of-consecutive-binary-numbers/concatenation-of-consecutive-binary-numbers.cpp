44e7360aa129e11620bc77966936d4ac62525646
Raghav Agrawal <codeworks2u@gmail.com>
2026-05-16 22:02:24 +0530

    Time: 48 ms (41.06%) | Memory: 8.2 MB (39.88%) - LeetSync

---
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numberofbits(int n){
+        return log2(n)+1;
+    }
+    int concatenatedBinary(int n) {
+        long ans=0,MOD=1e9+7;
+        for(int i=1;i<=n;i++){
+            int len=numberofbits(i);
+            ans=((ans<<len)%MOD+i)%MOD;
+        }
+        return ans;
+    }
+};
